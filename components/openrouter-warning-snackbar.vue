08241fbfeb7305795946826fc7cfbabf99c3ab42
zachMitchellAI <69049600+zachMitchellAI@users.noreply.github.com>
2026-09-14 19:27:56 -0400

    This should probably go in too

--- components/openrouter-warning-snackbar.vue
+++ components/openrouter-warning-snackbar.vue
@@ -1,49 +1,55 @@
 <style scoped>
 p {
-  text-align: center;
+    text-align: center;
 }
 a {
-  color: turquoise;
+    color: turquoise;
 }
 span {
-  padding: 0.5em;
+    padding: 0.5em;
 }
 </style>
 
 <template>
-  <VSnackbar
-    v-model="snackbar"
-    timeout="-1"
-    color="error"
-    prepend-icon="mdi-cancel"
-    rounded="pill"
-    variant="tonal"
-  >
-    <template>
-      <p>
-        <span
-          >Hey! This app needs an openrouter api key to run. Grab one by
-          <a href="https://openrouter.ai"
-            >Visiting their site and registering!</a
-          >
-          (See <code>.env</code> file in the root of this project)</span
-        >
-        <VBtn variant="tonal" @click="snackbar = false" color="red-darken-1">
-          Gotcha
-        </VBtn>
-      </p>
-    </template>
-  </VSnackbar>
+    <VSnackbar
+        v-model="snackbar"
+        timeout="-1"
+        color="error"
+        prepend-icon="mdi-cancel"
+        rounded="pill"
+        variant="tonal"
+    >
+        <template>
+            <p>
+                <span
+                    >Hey! This app needs an openrouter api key to run. Grab one
+                    by
+                    <a href="https://openrouter.ai"
+                        >Visiting their site and registering!</a
+                    >
+                    (Click the settings icon on the top-right corner to
+                    set)</span
+                >
+                <VBtn
+                    variant="tonal"
+                    @click="snackbar = false"
+                    color="red-darken-1"
+                >
+                    Gotcha
+                </VBtn>
+            </p>
+        </template>
+    </VSnackbar>
 </template>
 
 <script setup>
 const snackbar = ref(false);
 
 onMounted(async () => {
-  try {
-    snackbar.value = await fetchNeedsApiKey();
-  } catch (error) {
-    console.error("Failed to check API key status", error);
-  }
+    try {
+        snackbar.value = await fetchNeedsApiKey();
+    } catch (error) {
+        console.error("Failed to check API key status", error);
+    }
 });
 </script>
